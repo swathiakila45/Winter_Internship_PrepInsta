0c316365fd51b8b084fbe25ca5ac4104f386d939
swathiakila45 <59991732+swathiakila45@users.noreply.github.com>
2024-03-01 20:37:50 +0530

    Update data_cleaning_set2.py

--- Week3/data_cleaning_set2.py
+++ Week3/data_cleaning_set2.py
@@ -8,6 +8,7 @@ data = pd.read_csv("chipotle.tsv",delimiter='\t')
 
 #Creating a copy of the imported data 
 
+# print(data.head(1))
 df= data.copy()
 
 #Step1: Convert item_price to float
@@ -31,7 +32,72 @@ grouped_df = df.groupby('order_id').agg({
 }).reset_index()
 
 # Display the grouped DataFrame
-print(grouped_df.head(5))
+# print(grouped_df.head(5))
+
+# Quantity for Each Item and top 5 items with most quantity ordered 
+
+c=df.groupby('item_name').sum()
+c=c.sort_values(['quantity'],ascending=False)
+
+# print(c.head(5))
+# #Output
+#                      order_id  quantity                                 choice_description  item_price
+# item_name
+# Chicken Bowl           713926       761  [Tomatillo-Red Chili Salsa (Hot), [Black Beans...     7342.73
+# Chicken Burrito        497303       591  [Tomatillo-Green Chili Salsa (Medium), [Pinto ...     5575.82
+# Chips and Guacamole    449959       506                                                  0     2201.04
+# Steak Burrito          328437       386  [Tomatillo Red Chili Salsa, [Fajita Vegetables...     3851.43
+# Canned Soft Drink      304753       351  [Coke][Sprite][Coke][Coke][Lemonade][Sprite][D...      438.75
+
+#most ordered based on choice_description 
+
+cd=df.groupby('choice_description').sum()
+cd=cd.sort_values(['quantity'],ascending=False)
+# print(cd[['item_name','quantity']].head(1))
+# #Output
+#  item_name  quantity
+
+# [Diet Coke]      159
+
+#total orders in total 
+total_order= df.quantity.sum()
+print(total_order)
+
+#Output 4972
+
+#Total revenue 
+total_revenue = df.item_price.sum()
+print(total_revenue)
+
+#Output 34500.16
+
+#total orders made in the period 
+total_order_based_on_orderid = df.order_id.value_counts().count()
+print(total_order_based_on_orderid )
+
+#Output 1834 
+
+df['revenue']=df['quantity']*df['item_price']
+
+
+ar=df.groupby(by=['order_id']).sum()
+
+print(ar['revenue'].head(5))
+
+print(ar['revenue'].mean())
+
+
+#Output : 21.39423118865867
+
+#How many different items are sold 
+different_items_sold= df.item_name.nunique()
+print(different_items_sold)
+
+#Output : 50
+
+
+
+
 
 
 
